ad463c7ae3f828a68ca53c98c70c27697cd427a1
Illumi-z <zetanyu@gmail.com>
2022-02-01 18:34:21 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            project: {
+                title: 'Portfolio',
+                name: 'Henry Ybañez',
+                email: 'htybanez1974@gmail.com'
+            }
+        }
+    }
+})
+
+app.mount('#app')
